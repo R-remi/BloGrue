f81c5eb17b7e5a034269111401c178f7ed46648e
Rémi <remr04@gmail.com>
2025-10-10 13:31:28 +0200

    modif data.php move to about

--- about.php
+++ about.php
@@ -1,42 +1,34 @@
 <?php
-$currentPage = 'about';
-$pageTitle = 'about - Projet grue';
+$currentPage = 'data';
+$pageTitle = 'Données et mesures - Projet grue';
 
 include 'header.php';
 ?>
 
 <section class="content-page">
-    <h1>A propos de la grue</h1>
+    <h1>Données et mesures</h1>
 
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur.</p>
+    <h2>1. Calcul de la Charge : 300 grammes</h2>
+    <p>
+        Pour simuler une charge réelle de 300 kg sur notre maquette à l'échelle 1:10, nous avons dû réduire la masse par le cube de l'échelle (10³ = 1000), car le poids dépend du volume.
+    </p>
+    <p>
+        La charge imposée étant de 340g, nous avons calculé la force correspondante en Newton : Wc = mc * g = 0.340 * 9.81 = 3.3354 N.
+    </p>
+    <h2>2. L'Équilibre des Moments</h2>
+    <p>
+        Pour ne pas basculer, la grue doit équilibrer deux forces opposées, comme sur une balançoire. C'est le principe des moments.
+    </p>
+    <ul>
+        <li><strong>Moment Renversant :</strong> La force de la charge de 300 g qui tente de faire basculer la grue.</li>
+        <li><strong>Moment Stabilisant :</strong> La force du poids de la structure de la grue qui la maintient au sol.</li>
+    </ul>
+    <p>
+        Pour que notre grue soit stable, il faut que le <strong>moment stabilisant soit toujours supérieur au moment renversant.</strong> Nos calculs ont validé que cette condition est respectée, garantissant que notre grue ne basculera pas (théoriquement).
+    </p>
+    <img src="img/calculs.jpeg" alt="Photo des calculs manuscrits" class="hero-image"">
 
-    <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
-        aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
 
-    <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-        eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
-        consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam
-        quaerat voluptatem.</p>
-
-    <p>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea
-        commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae
-        consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
-
-    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque
-        corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in
-        culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.</p>
-
-    <p>Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio
-        cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor
-        repellendus.</p>
-
-    <p>Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates
-        repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut
-        reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
 </section>
 
 <?php include 'footer.php'; ?>
