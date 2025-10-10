9097ced0ab86b27cc329479ac865fc04dcdd81be
Rémi <remr04@gmail.com>
2025-10-10 11:41:15 +0200

    add logoss

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo isset($pageTitle) ? $pageTitle : 'Projet grue'; ?></title>
     <link rel="stylesheet" href="style.css">
-
+    <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-WV60FWGCKP"></script>
     <script>
