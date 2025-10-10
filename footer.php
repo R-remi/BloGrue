c000eac70a778f217e986b53ef18693630abc060
Rémi <remr04@gmail.com>
2025-10-10 13:42:11 +0200

    foot + logo

--- footer.php
+++ footer.php
@@ -1,8 +1,12 @@
 </main>
 
 <footer>
-    <p>&copy; 2025 Projet grue. Tous droits réservés.</p>
+    <div class="footer-content">
+        <img src="/img/logo_cesi.jpg" alt="Logo CESI" class="footer-logo">
+        <p>&copy; 2025 Projet grue. Tous droits réservés.</p>
+        <img src="/img/logo.png" alt="Logo Projet" class="footer-logo">
+    </div>
 </footer>
-</body>
 
+</body>
 </html>
